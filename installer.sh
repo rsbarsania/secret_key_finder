427ecd6aa878f53c6995aded51be2e385cad2cf7
rsbarsania <81644857+rsbarsania@users.noreply.github.com>
2021-12-19 00:16:35 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get install -y curl grep python3 python3-pip git
+
+chmod +x *.sh
+
+mkdir -p ~/tools
+
+cd ~/tools
+
+git clone https://github.com/GerbenJavado/LinkFinder.git && cd Linkfinder && python3 setup.py install && pip3 install -r requirements.txt
+
+cd ~/tools
+
+git clone https://github.com/m4ll0k/SecretFinder.git secretfinder && cd secretfinder && pip3 install -r requirements.txt
+
