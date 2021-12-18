693cb751f85aec5ed0502fa4cc26dacb9c857f34
rsbarsania <81644857+rsbarsania@users.noreply.github.com>
2021-12-19 00:41:23 +0530

    Update secret_key_finder.sh

--- secret_key_finder.sh
+++ secret_key_finder.sh
@@ -5,7 +5,7 @@ tar=$1
 echo -e "\e[33m[+] Fetching URL's From Target\e[0m"
 curl -s "$tar" > urls-resp.log
 curl -s "$tar" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep -i "\.js\|\.svg\|\.css\|\.xml\|\.html" | tee urls.log
-
+echo "$tar" | tee -a urls.log
 echo -e "\n\e[33m[+] Please wait... While urls's validation is in process\e[0m"
 while IFS= read Url; do curl "$Url" -s; done < urls.log >> urls-resp.log
 
